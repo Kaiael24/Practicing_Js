f745e12efec5346cfeb8883df327a8eebfd23bf4
Kaiael24 <juanchii10p@gmail.com>
2023-05-21 18:38:14 -0300

    Funciones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Variables y tipo de datos */
+
 console.log("Practicando Js")
 
 let num1 = 50;
@@ -20,3 +22,35 @@ let estudiandoAndo = "Practicando JavaScript";
 console.log(num1);
 console.log(num2);
 console.log(saludo + " " + miNombre + " y estoy " + estudiandoAndo )
+
+let soyVerdadero = true;    
+let soyFalso = false;
+let soyNulo = null;
+let soyUndefined; // es undefined;
+
+console.log(soyVerdadero);
+console.log(soyFalso);
+console.log(soyNulo);
+console.log(soyUndefined);
+
+/* Funciones */
+
+/* 
+La función debe recibir un número y mostrar en
+la consola una cuenta regresiva desde ese
+número hasta el cero. Además, cuando la cuenta
+llegue a cero, en la consola se mostrará el
+mensaje ¡Despegamos!
+Por ejemplo, si a esta función le paso el número
+10.
+*/
+
+function despegando(parametro) {
+    for (let i = parametro; i >= 0; i--){
+        console.log(i)
+        if(i == 0){
+            console.log("Despegamos!!")
+        }       
+    }
+}   
+despegando(10)
