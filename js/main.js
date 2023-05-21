86d54d6b2d0cd5dc9a6e7ee47b258f8850594273
Kaiael24 <juanchii10p@gmail.com>
2023-05-21 18:05:55 -0300

    practicando Js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+console.log("Practicando Js")
+
+let num1 = 50;
+let num2 = 10;
+let saludo = '¡Hola!';
+let nombreCurso = 'Código LATAM';
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2;
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + 'Esto es ' + nombreCurso);
+
+num1 = 20 - 5 + num2;
+num2 =  saludo + resultadoSuma;
+let miNombre = "Soy Juan Pablo";
+let estudiandoAndo = "Practicando JavaScript";
+
+console.log(num1);
+console.log(num2);
+console.log(saludo + " " + miNombre + " y estoy " + estudiandoAndo )
